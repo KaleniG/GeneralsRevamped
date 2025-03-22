e2df4cc60cac81f3c58f5062a5ffb0d77a5c993d
KaleniG <151833811+KaleniG@users.noreply.github.com>
2025-03-22 18:30:32 +0100

    Added Premake5 build system (Windows-only for now)
    - The initial workspace structure consists of the 'SAGE' project, which serves as the game engine for both 'Generals' and 'GeneralsZeroHour,' the game implementations.

---
+++ premake5.lua
@@ -0,0 +1,40 @@
+WORKSPACEDIR = _WORKING_DIR
+OUTPUTDIR = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+workspace "GeneralsRevamped"
+  startproject "GeneralsZeroHour"
+
+  configurations 
+  { 
+    "Debug",
+    "Release",
+    "Final"
+  }
+
+  platforms 
+  { 
+    "Win64",
+    "Win32"
+  }
+
+  filter { "platforms:Win64" }
+    system "Windows"
+    systemversion "latest"
+    architecture "x86_64"
+
+  filter { "platforms:Win32" }
+    system "Windows"
+    systemversion "latest"
+    architecture "x86"
+
+group "Dependencies"
+group ""
+
+group "Core"
+  include "SAGE"
+group ""
+
+group "Implementations"
+  include "Generals"
+  include "GeneralsZeroHour"
+group ""
