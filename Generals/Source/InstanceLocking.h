ef9459f05bf3cda14d2aa6271df02784a1e483b4
KaleniG <151833811+KaleniG@users.noreply.github.com>
2025-03-23 12:48:46 +0100

    Updated the 'Generals' project
    - Both the 'Generals' and 'GeneralsZeroHour' need testing for the features introduced until now

--- Generals/Source/InstanceLocking.h
+++ Generals/Source/InstanceLocking.h
@@ -4,6 +4,7 @@
 
 namespace gen
 {
+  void LockInstance(bool value);
   bool LockInstanceAndCheck(const std::wstring& id);
   void FocusExistingInstanceWindow(const std::wstring& name);
   void UnlockInstance();
